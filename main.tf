76e55ea97a7dfd810517df8bc47c9c41ffb74bf4
JoEwMa <117019740+JoEwMa@users.noreply.github.com>
2023-03-31 23:01:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@ resource "google_storage_bucket" "static-site" {
   name          = "add-bucket"
   location      = "US"
 
-  uniform_bucket_level_access = true
-
   website {
     main_page_suffix = "index.html"
     not_found_page   = "404.html"
